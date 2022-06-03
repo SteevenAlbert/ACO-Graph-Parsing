bb233b13cc21b9306c1f1a18a8cea8678f8e9e2d
judykhairalla <judykhairalla@gmail.com>
2022-06-03 22:52:05 +0200

    Ant path added

--- ACO_Test.py
+++ ACO_Test.py
@@ -8,7 +8,12 @@ distances = np.array([[0, 2, 1, 5, 7],
                       [2, 4, 0, 1, 3],
                       [5, 8, 1, 0, 2],
                       [7, 2, 3, 2, 0]])
-                      
+
+# np.array([[0, 0.8, 0.2, 0.1, 0.5],
+#                       [0.2, 0, 0.3, 0.7, 0.2],
+#                       [0.5, 0.3, 0, 0.5, 0.4],
+#                       [0.6, 0.2, 0.6, 0, 0.3],
+#                       [0.5, 0.1, 0.4, 0.3, 0]])
 phermones =  np.ones(distances.shape)
 
 def find_start_ants(distances):
@@ -17,18 +22,49 @@ def find_start_ants(distances):
     start_ants = np.where(row_sum >= sum_mean)
     
     return start_ants
-  
-def ant_tour(distances):
-    path = []
-    row_mean = distances.mean(axis=1)
-    for i in range(distances.shape[0]):
-        for j in range(distances.shape[1]):
-            if(distances[i][j]>=row_mean[i]):
-                path.append([i,j])
+
+row_mean = distances.mean(axis=1)
+
+def all_paths(distances, start_ants):
+    for i in range(start_ants):
+        arr = []
+        path = ant_path(distances, start_ants[i], arr)
 
 
+def ant_path(distances, start_ant, path):
 
-start_ants = find_start_ants(distances)
-ant_tour(distances, start_ants)
+    if len(path) == distances.shape[0]:
+        return
+
+    mean = row_mean[start_ant]
+    row = distances[start_ant]
+    sorted = np.sort(distances[start_ant])[::-1]
+
+    for i in range(len(sorted)):
+        if sorted[i] >= mean and not any(np.where(row == sorted[i])[0][0] in sublist for sublist in path):
+            path.append([start_ant, np.where(row == sorted[i])[0][0]])
+            ant_path(distances, np.where(row == sorted[i])[0][0], path)
+            break
+
+# PROMISING/LEARNING RATE
+# promising(start_ant, np.where(row == sorted[i])[0][0], path)
+# def promising(start, end, path):
+#     sum = 0
+#     for i in range(len(path)):
+#         sum += distances[path[i][0]][path[i][1]]
+#     lr = sum/len(path)
+
+#     new_sum = sum + distances[start][end]
+#     new_lr = new_sum/(len(path)+1)
+
+#     if not any(end in sublist for sublist in path) and new_lr >= lr:
+#         return True
+#     return False
+    
+
+# start_ants = find_start_ants(distances)
+path = [[-1,-1]]
+ant_path(distances, 0, path)
+print(path)
 
 
