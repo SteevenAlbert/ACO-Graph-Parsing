6e8dfc59d1fcdac58ae50ff9956b8fc120d1ec8e
laila-elhattab <liko.hani@gmail.com>
2022-06-09 21:59:48 +0200

    new starting ants

--- ACO.py
+++ ACO.py
@@ -19,73 +19,111 @@ distances = np.array([[0, 2, 1, 5, 7],
 # distances = pd.read_csv("graphs\CryptoWall.csv_2_Win32_Filecoder.CryptoWall.G trojan.csv")
 
 
-phermones =  np.ones(distances.shape)
+phermones = np.ones(distances.shape)
+# row = [1, 2, 3, 4]
+# sorted = np.sort(row)[::-1]
+# print(np.where(row == sorted[0])[0][0])
+
 
 def find_start_ants(distances):
-    row_sum = distances.sum(axis=1)
-    sum_mean = np.mean(row_sum)
-    start_ants = np.where(row_sum >= sum_mean)
-    
+    start_ants = []
+    row_mean = distances.mean(axis=1)
+    for i in range(distances.shape[0]):
+        for j in range(distances.shape[1]):
+            if distances[i][j] >= row_mean[i]:
+                start_ants.append([i, j])
+    # for row in distances:
+    #     row_mean = row.mean()
+    #     for ele in row:
+    #         start_ants = np.where(ele >= row_mean)
     return start_ants
 
+
 row_mean = distances.mean(axis=1)
+# print(row_mean[1])
+
 
 def spread_phermone(path):
-    for i in range(path):
-        phermones[path[i][0]][path[i][1]]*=1/distances[path[i][0]][path[i][1]]
+    for i in range(len(path)):
+        if distances[path[i][0]][path[i][1]] != 0:
+            phermones[path[i][0]][path[i][1]] *= 1 / \
+                distances[path[i][0]][path[i][1]]
+
+
+def promising(start, end, path):
+    sum = 0
+    for i in range(len(path)):
+        sum += distances[path[i][0]][path[i][1]]
+    lr = sum/len(path)
+
+    new_sum = sum + distances[start][end]
+    new_lr = new_sum/(len(path)+1)
+
+    if not any(end in sublist for sublist in path) and new_lr >= lr:
+        return True
+    return False
+
 
 def all_paths(distances, start_ants):
-    paths=[]
+    paths = []
     for i in range(len(start_ants)):
-        path = [[-1,-1]]
+        path = [[-1, -1]]
         ant_path(distances, start_ants[i], path)
         paths.append(path)
-    for j in range(paths):
+    for j in range(len(paths)):
         spread_phermone(paths[j])
-
+    return paths
 
 
 def ant_path(distances, start_ant, path):
-
+    val = False
     if len(path) == distances.shape[0]:
         return
 
-    mean = row_mean[start_ant]
-    row = distances[start_ant]*phermones[start_ant]
-    sorted = np.sort(distances[start_ant])[::-1]
+    # mean = row_mean[start_ant[1]]
+    row = distances[start_ant[1]]
+    sorted = np.sort(row)[::-1]
 
+    # for i in range(len(sorted)):
+    #     if sorted[i] >= mean and not any(np.where(row == sorted[i])[0][0] in sublist for sublist in path):
+    #         path.append([start_ant, np.where(row == sorted[i])[0][0]])
+    #         ant_path(distances, np.where(row == sorted[i])[0][0], path)
+    #         break
+    path.append(start_ant)
     for i in range(len(sorted)):
-        if sorted[i] >= mean and not any(np.where(row == sorted[i])[0][0] in sublist for sublist in path):
-            path.append([start_ant, np.where(row == sorted[i])[0][0]])
-            ant_path(distances, np.where(row == sorted[i])[0][0], path)
+        if promising(start_ant[1], np.where(row == sorted[i])[0][0], path):
+            # path.append([start_ant[1], np.where(row == sorted[i])[0][0]])
+            ant_path(distances, [start_ant[1], np.where(
+                row == sorted[i])[0][0]], path)
             break
+            # if sorted[i] >= mean:  # no error
+            #     for path1 in path:
+            #         if sorted[i] == path1[1]:
+            #             val = True
+            #     if val:
+            #         path.append([start_ant, sorted[i]])
+            #         ant_path(distances, sorted[i], path)
+            #         break
+# ants = find_start_ants(distances)
+# ant_path(distances, ants, path)
 
 
-def run(iterations):
+def run(iterations, phermones):
     for i in range(iterations):
-        ants=find_start_ants(distances)
-        all_paths(distances,ants)
-        phermones*=0.95
+        ants = find_start_ants(distances)
+        print(all_paths(distances, ants))  # [-1,-1]??
+        phermones *= 0.95
+
+
+run(3, phermones)
+# print(phermones)
+
+
 # PROMISING/LEARNING RATE
 # promising(start_ant, np.where(row == sorted[i])[0][0], path)
-# def promising(start, end, path):
-#     sum = 0
-#     for i in range(len(path)):
-#         sum += distances[path[i][0]][path[i][1]]
-#     lr = sum/len(path)
 
-#     new_sum = sum + distances[start][end]
-#     new_lr = new_sum/(len(path)+1)
-
-#     if not any(end in sublist for sublist in path) and new_lr >= lr:
-#         return True
-#     return False
-    
 
 # start_ants = find_start_ants(distances)
-path = [[-1,-1]]
-ant_path(distances, 0, path)
+# path = [[-1, -1]]
+# ant_path(distances, 0, path)
 # print(path)
-# run(3) 
-
-
